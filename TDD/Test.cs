7b8e4231916a8ccb3fcc66b8ba32db3cf91d8309
SergeyGorbanenko <xxx1607xxx@mail.ru>
2018-04-12 20:39:35 +0700

    Создали тест на проверку транспонированной матрицы Test4_Trans.

--- TDD/Test.cs
+++ TDD/Test.cs
@@ -9,6 +9,7 @@ namespace TDD
     {
         private double[,] a = { { 1, 2, 3 }, { 2, 5, 6 }, { 4, 2, 7 } };
         private double[,] b = { { 5, 3, 1 }, { 2, 8, 0 }, { 3, 5, 4 } };
+        private double[,] t = { { 1, 2, 4 }, { 2, 5, 2 }, { 3, 6, 7 } };
 
         [TestMethod]
         public void Test1_IsNotNullMathCalc()
@@ -31,6 +32,14 @@ namespace TDD
             MathCalc calc = new MathCalc();
             bool rezult = calc.CompareMatrix(a, b);
             Assert.AreEqual(false, rezult);
+        }
+
+        [TestMethod]
+        public void Test4_Trans()
+        {
+            MathCalc calc = new MathCalc();
+            double [,] rezult = calc.Trans(a);
+            Assert.AreEqual(true, calc.CompareMatrix(t, rezult));
         }
     }
 }
