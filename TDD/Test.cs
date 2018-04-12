8dd037d88c6b4d874a8b3bd6ae285812f46c8d44
SergeyGorbanenko <xxx1607xxx@mail.ru>
2018-04-12 20:33:51 +0700

    Создали тест на сравнение разных матриц Test3_CompareMatrix. Добавили матрицу b.

--- TDD/Test.cs
+++ TDD/Test.cs
@@ -8,6 +8,7 @@ namespace TDD
     public class Test
     {
         private double[,] a = { { 1, 2, 3 }, { 2, 5, 6 }, { 4, 2, 7 } };
+        private double[,] b = { { 5, 3, 1 }, { 2, 8, 0 }, { 3, 5, 4 } };
 
         [TestMethod]
         public void Test1_IsNotNullMathCalc()
@@ -22,6 +23,14 @@ namespace TDD
             MathCalc calc = new MathCalc();
             bool rezult = calc.CompareMatrix(a, a);
             Assert.AreEqual(true, rezult);
+        }
+
+        [TestMethod]
+        public void Test3_CompareMatrix()
+        {
+            MathCalc calc = new MathCalc();
+            bool rezult = calc.CompareMatrix(a, b);
+            Assert.AreEqual(false, rezult);
         }
     }
 }
