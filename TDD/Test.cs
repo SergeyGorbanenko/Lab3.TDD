51c7abb4488ab0e7eea0eb3da4d711090e29a2e7
SergeyGorbanenko <xxx1607xxx@mail.ru>
2018-04-12 20:45:02 +0700

    Создали тест на сложение двух матриц Test4_Add.

--- TDD/Test.cs
+++ TDD/Test.cs
@@ -10,6 +10,7 @@ namespace TDD
         private double[,] a = { { 1, 2, 3 }, { 2, 5, 6 }, { 4, 2, 7 } };
         private double[,] b = { { 5, 3, 1 }, { 2, 8, 0 }, { 3, 5, 4 } };
         private double[,] t = { { 1, 2, 4 }, { 2, 5, 2 }, { 3, 6, 7 } };
+        private double[,] d = { { 6, 5, 4 }, { 4, 13, 6 }, { 7, 7, 11 } };
 
         [TestMethod]
         public void Test1_IsNotNullMathCalc()
@@ -40,6 +41,14 @@ namespace TDD
             MathCalc calc = new MathCalc();
             double [,] rezult = calc.Trans(a);
             Assert.AreEqual(true, calc.CompareMatrix(t, rezult));
+        }
+
+        [TestMethod]
+        public void Test5_Add()
+        {
+            MathCalc calc = new MathCalc();
+            double[,] rezult = calc.Add(a, b);
+            Assert.AreEqual(true, calc.CompareMatrix(rezult, d));
         }
     }
 }
