ebd9cd160609eca64e983df8744dedbca925d355
SergeyGorbanenko <xxx1607xxx@mail.ru>
2018-04-12 20:20:45 +0700

    Через "Быстрые действия и рефакторинг" автоматически создали метод CompareMatrix

--- Lab3TDD/MathCalc.cs
+++ Lab3TDD/MathCalc.cs
@@ -1,9 +1,14 @@
-﻿namespace Lab3TDD
+﻿using System;
+
+namespace Lab3TDD
 {
     public class MathCalc
     {
         public MathCalc() { }
 
-
+        public bool CompareMatrix(double[,] a1, double[,] a2)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
